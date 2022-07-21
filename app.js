fa955c80c971af6c442ee4784ac113a696681a19
Grey Altr <grey.sifr@gmail.com>
2022-07-20 21:35:44 -0700

    js-works-event-listener-is-in

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // import functions and grab DOM elements
+const nameEl = document.getElementById('name');
+const pronounsEl = document.getElementById('pronouns');
+const buttonEL = document.getElementById('submit');
 
 // let state
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// get user input
+// use user input to update state 
+// update DOM to reflect the new state
+buttonEL.addEventListener('click', () => {
+  const nameInputEl = document.getElementById('your-name');
+  const pronounsInputEl = document.getElementById('your-pronouns');
+  nameEl.textContent = nameInputEl.value;
+  pronounsEl.textContent = pronounsInputEl.value;
+});
