f93a27180763c242235ad6214b5635f6d24b834e
Grey Altr <grey.sifr@gmail.com>
2022-07-20 21:52:31 -0700

    color-change-works

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const nameEl = document.getElementById('name');
 const pronounsEl = document.getElementById('pronouns');
 const buttonEL = document.getElementById('submit');
+const shadeEl = document.getElementById('shade');
+const glyphEl = document.getElementById('glyph');
 
 // let state
 
@@ -14,4 +16,6 @@ buttonEL.addEventListener('click', () => {
   const pronounsInputEl = document.getElementById('your-pronouns');
   nameEl.textContent = nameInputEl.value;
   pronounsEl.textContent = pronounsInputEl.value;
+  document.getElementById('top').style.backgroundColor = shadeEl.value;
+  document.getElementById('bottom').style.backgroundColor = shadeEl.value;
 });
